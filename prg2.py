786e54c849b396ea4b7a903165d03e90aaef6194
bhagyamuduguri70-ops <bhagyamuduguri70@gmail.com>
2025-11-15 12:30:19 +0530

    prg2.py

---
+++ prg2.py
@@ -0,0 +1,8 @@
+# Define the employee details
+employee_id = "292"
+employee_name = "bhagyashree muduguri"
+employee_salary = 95000.00 
+# Print the details
+print("Employee ID is ", employee_id)
+print("Employee Name is ", employee_name)
+print("Employee Salary is $", employee_salary)
